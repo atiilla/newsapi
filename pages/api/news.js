7edab1a6698ad68d46fa29ca4efc460bda6bf2b3
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-10-03 22:18:24 +0200

    init

---
+++ pages/api/news.js
@@ -0,0 +1,9 @@
+import axios  from 'axios';
+export default function handler(req, res) {
+    const {apiKey,country,category} = req.query;
+    axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`)
+    .then(response=>{
+        res.status(200).json(response.data);
+    })
+ // res.status(200).json({ name: 'John Doe' })
+}
