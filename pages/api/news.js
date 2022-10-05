2b75f3b485f714a01f2eb955941a3287aecc4061
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-10-05 19:06:43 +0200

    updated

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,7 +1,7 @@
 import axios  from 'axios';
 export default function handler(req, res) {
-    const {apiKey,country,category} = req.query;
-    axios.get(`https://arkakapi.herokuapp.com/https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`)
+    const {apiKey,country,category,page,pageSize} = req.query;
+    axios.get(`https://arkakapi.herokuapp.com/https://newsapi.org/v2/top-headlines?country=${country}&page=${page}&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`)
     .then(response=>{
         res.status(200).json(response.data);
     })
